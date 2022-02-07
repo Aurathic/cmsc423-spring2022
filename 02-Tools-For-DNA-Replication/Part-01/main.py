3589a7a0510ddef65b5ac1547fcbe35967e7ee5f
Adam <39442291+Aurathic@users.noreply.github.com>
2022-02-07 15:21:32 -0500

    Fix part 1 of project 2

--- 02-Tools-For-DNA-Replication/Part-01/main.py
+++ 02-Tools-For-DNA-Replication/Part-01/main.py
@@ -1,13 +1,9 @@
-
 with open("./input","r") as fin, open("./output","w") as fout:
     query = fin.readline()[:-1]
-    text = fin.readline()[:-1]
-    print(query)
-    print(text)
+    text = fin.read().replace("\n","")
     matches = []
     for i in range(len(text)-len(query)+1):
-        print(text[i:i+len(query)] + " == " + query)
+        #print(text[i:i+len(query)] + " == " + query)
         if text[i:i+len(query)] == query:
-            print(i)
             matches.append(str(i))
     fout.write(" ".join(matches))
