49807ae8e9dff8840274140f9465c2576f373510
Adam <39442291+Aurathic@users.noreply.github.com>
2022-02-09 16:47:03 -0500

    Rewrite Part 01 of Project 2 to use my own string matching code
    It's not efficient, but that's not the point

--- 02-Tools-For-DNA-Replication/Part-01/main.py
+++ 02-Tools-For-DNA-Replication/Part-01/main.py
@@ -1,9 +1,14 @@
+def match(str1, str2):
+    for c1, c2 in zip(str1, str2):
+        if c1 != c2:
+            return False
+    return True
+
 with open("./input","r") as fin, open("./output","w") as fout:
     query = fin.readline()[:-1]
     text = fin.read().replace("\n","")
     matches = []
     for i in range(len(text)-len(query)+1):
-        #print(text[i:i+len(query)] + " == " + query)
-        if text[i:i+len(query)] == query:
+        if match(text[i:i+len(query)],query):
             matches.append(str(i))
     fout.write(" ".join(matches))
