12cdad5406287a9a1f5b4dcd245ab35a2aa40352
Adam <39442291+Aurathic@users.noreply.github.com>
2022-02-18 23:24:43 -0500

    Fix errors, add some extra code stuffs

--- 04-Greedy-Motif-Search-With-Laplace/main.py
+++ 04-Greedy-Motif-Search-With-Laplace/main.py
@@ -27,6 +27,20 @@ def merged_cols(motifs):
     for i in range(len(motifs[0])): # Assumes all are the same length
         yield "".join([t[i] for t in motifs])
 
+def pseudocount(motifs):
+    t = len(motifs)
+    k = len(motifs[0]) # Assuming all are same length
+    mat = []
+    letters = ['A','C','G','T']
+    for ind_str in merged_cols(motifs):
+        pr = []
+        for c in letters:
+            # This is probably not an efficient way to do it, but whatever
+            count = ind_str.count(c)
+            pr.append(count+1)
+        mat.append(pr)
+    return mat
+
 def prob_profile(motifs, laplace_rule=True):
     """Input is a list of DNA sequences of length k.
     Output a k by 4 matrix of the proportions of each
@@ -43,7 +57,7 @@ def prob_profile(motifs, laplace_rule=True):
             # This is probably not an efficient way to do it, but whatever
             count = ind_str.count(c)
             if laplace_rule:
-                pr.append((count+1)/(2*t))
+                pr.append((count+1)/(t+4))
             else:
                 pr.append(count/t)
         mat.append(pr)
@@ -59,16 +73,27 @@ def consensus_score(motifs):
         accum += (t - most_common_freq)
     return accum
 
-def entropy(motifs):
+def entropy(motifs, laplace_rule=False):
     """Input is a list of k-mers.
     Return the sum of the entropy for each row of
     the matrix. 
     """
     # Replace all 0s with 1s in order to avoid domain error on log calculation
-    mat = [1 if x==0 else x for x in chain(*prob_profile(motifs, laplace_rule=False))]
+    mat = None 
+    if laplace_rule:
+        mat = chain(*prob_profile(motifs, laplace_rule=True))
+    else:
+        mat = [1 if x==0 else x for x in chain(*prob_profile(motifs, laplace_rule=False))]
     #print(mat)
     #print(list(-sum(x*log(x,2) for x in row) for row in mat))
     return -sum(x*log(x,2) for x in mat)
+
+def prob(motifs, seq, laplace_rule=True):
+    mat = prob_profile(motifs, laplace_rule)
+    kmer_prob = 1
+    for j in range(len(seq)):
+        kmer_prob *= mat[j][dna_ind_map[seq[j]]]
+    return kmer_prob
 
 def most_probable_kmer(seq, k, mat):
     """Given the probability profile matrix
@@ -94,6 +119,7 @@ def window(s, k):
         yield s[i:i+k]
 
 ## Driver code
+"""
 with open("./input","r") as fin, open("./output","w") as fout:
     k, t = [int(x) for x in fin.readline().split()]
     seqs = fin.readlines()
@@ -110,6 +136,7 @@ with open("./input","r") as fin, open("./output","w") as fout:
             best_motifs = new_motifs
             best_score = new_score
     fout.write("\n".join(best_motifs))
+"""
 
 def compare_results():
     calculated = """ATT
@@ -126,3 +153,32 @@ def compare_results():
 
     print(f"given {entropy(given)}")
     print(f"calculated {entropy(calculated)}")
+
+def assessment_code():
+    x = """TAAC
+    GTGT
+    CCGG
+    ACTA
+    AGGT
+    ATTT""".split()
+    print(pseudocount(x))
+    print(prob(x, "GACT"))
+
+    y = """ACGGAC
+    CAGCGA
+    CCGGAC
+    CGGTCA
+    TTGAAC""".split()
+    print(prob_profile(y, laplace_rule=False))
+
+    z = """A
+    C
+    G
+    T
+    C
+    G
+    C
+    T
+    C
+    T""".split()
+    print(entropy(z))
