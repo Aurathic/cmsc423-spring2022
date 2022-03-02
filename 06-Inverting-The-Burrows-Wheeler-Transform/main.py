e65cbcc75071fae5caea9feb8b4fbf89d9da51f0
Adam <39442291+Aurathic@users.noreply.github.com>
2022-03-02 16:31:53 -0500

    First pass at inverse BWT

---
+++ 06-Inverting-The-Burrows-Wheeler-Transform/main.py
@@ -0,0 +1,52 @@
+from itertools import chain
+#https://www.geeksforgeeks.org/inverting-burrows-wheeler-transform/
+
+def reverse_bwt(bwt):
+
+    first_col = sorted(list(bwt))
+
+    order_map = dict()
+    order_map['$'] = []
+    #print(bwt)
+
+    bwt_copy = list(bwt)
+    """
+    for i,c in enumerate():
+        if c in order_map:
+            print(f"append {i} to order_map[{c}]")
+            order_map[c].append(i)
+        else:
+            order_map[c] = [i]
+    """
+
+    for c in first_col:
+        i = bwt_copy.index(c)
+        if c in order_map:
+            order_map[c].append(i)
+        else:
+            order_map[c] = [i]
+        bwt_copy[i] = '$' # TODO Really inefficient
+
+    #print(order_map)
+    first_col_indexes = list(chain(*order_map.values()))
+    #print(list(zip(enumerate(first_col_indexes),list(bwt))))
+
+    ret = []
+    ind = first_col_indexes[0]
+    for i in range(len(bwt)):
+        ind = first_col_indexes[ind]
+        ret += bwt[ind]
+
+    return "".join(ret)
+
+def main():
+    with open("./input","r") as fin, open("./output","w") as fout:
+        text = fin.readline()
+        sout = reverse_bwt(text)
+        fout.write(sout)
+
+    main()
+
+text1 = "annb$aa"
+text2 = "TTCCTAACG$A"
+print(reverse_bwt(text2))
