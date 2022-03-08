046205bf530aa156750f5e35cb10720103770267
Adam <39442291+Aurathic@users.noreply.github.com>
2022-03-08 13:33:55 -0500

    Attempt to try a different approach for storing backtracking data

--- 07-Find-The-Highest-Scoring-Alignment-Of-Two-Strings/main.py
+++ 07-Find-The-Highest-Scoring-Alignment-Of-Two-Strings/main.py
@@ -1,14 +1,31 @@
 
+def get_prev_ind(curr_ind, dir):
+    """
+    Get the previous index tuple for the matrix based on the current index and a number
+    which determines which direction to traverse (0 means diagonally up & left, 1 means
+    above, 2 means left)
+    """
+    (i,j) = curr_ind
+    if dir == 0:
+        return (i-1,j-1)
+    elif dir == 1:
+        return (i-1,j)
+    elif dir == 2:
+        return (i,j-1)
+    else:
+        raise Exception(f"direction id '{dir}' is not valid (must be 0, 1, or 2)")
+
 def highest_score_cell(i, j, s1, s2, mat):
     """
     Assumes 1 <= i < m, 1 <= j <= n
     """
-    prev_ind_score = [
-        ((i-1,j-1), mat[i-1][j-1]+blosum_score(i,j,s1,s2)),
-        ((i-1,j),   mat[i-1][j]+gap_penalty),
-        ((i,j-1),   mat[i][j-1]+gap_penalty)
+    poss_score = [
+        mat[i-1][j-1]+blosum_score(i,j,s1,s2), # from diagonal
+        mat[i-1][j]+gap_penalty, # from above
+        mat[i][j-1]+gap_penalty # from left
     ]
-    return max(prev_ind_score, key = lambda k: k[1]) # Only returns one of the possible max values; this should be fine though
+    high_score = max(poss_score)
+    return (high_score, poss_score.index(high_score)) # Only returns one of the possible max values; this should be fine though
 
 def blosum_score(i, j, s1, s2):
     """
@@ -22,15 +39,15 @@ def highest_score_alignment(s1, s2):
     highest_score = 0
 
     mat = [[0 for j in range(n+1)] for i in range(m+1)] # used for dynamic programming
-    edge_list = dict() # Store the value from which the current cell value was derived
+    edge_dir_list = dict() # Store the value from which the current cell value was derived
 
     # Filling in matrix, getting highest score
     for i in range(1,m+1):
         for j in range(1,n+1):
-            prev_ind, score = highest_score_cell(i, j, s1, s2, mat)
+            prev_dir, score = highest_score_cell(i, j, s1, s2, mat)
             mat[i][j] = score
             print(f"mat[{i}][{j}] = {score}")
-            edge_list[(i,j)] = prev_ind
+            edge_dir_list[(i,j)] = prev_dir # did we get to this cell from above-left, above, or left?
     print(mat)
 
     highest_score = mat[m][n]
@@ -39,10 +56,10 @@ def highest_score_alignment(s1, s2):
     align1, align2 = "", ""
     curr_ind = (m,n)
     while curr_ind != (0,0):
-        # TODO
-        curr_ind = edge_list[curr_ind]
-    
-
+        curr_dir = edge_dir_list[curr_ind] 
+        # TODO turn the direction into useful information about what transformation occurred
+        # I think the original way I did this was better actually, whoops
+        curr_ind = get_prev_ind(curr_ind, curr_dir)
     return [str(highest_score), align1, align2]
 
 def main():
